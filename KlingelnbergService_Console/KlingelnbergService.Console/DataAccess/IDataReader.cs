e1d4656ca9f510e294f0a1287e7efc99188326f7
MakandarSahil <sahilmakandar15@gmail.com>
2025-12-29 18:14:22 +0530

    main branch updated

---
+++ KlingelnbergService_Console/KlingelnbergService.Console/DataAccess/IDataReader.cs
@@ -0,0 +1,16 @@
+﻿using KlingelnbergService.Models;
+using System;
+using System.Collections.Generic;
+
+namespace KlingelnbergService.DataAccess
+{
+    // interface for reading machine asset mapping data from various sources
+    // interface will help to not change other files rather than I/O files 
+    public interface IDataReader
+    {
+        List<MachineAssestMapping> ReadData();
+    }
+}
+
+// if data source changes we can add new readers 
+// as of now we have TextFile data reader
